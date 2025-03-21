ff764c2b9097f2c552185882607a417e817ffa43
LoiLeVan2 <leloicoi123@gmail.com>
2025-03-21 18:36:38 +0700

    Compelte

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { listOrders, placeOder, updateStatus, userOrders, verifyOrder } from "../controllers/orderController.js";
+import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from "../controllers/orderController.js";
 import authMiddleware from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
-orderRouter.post("/place", authMiddleware, placeOder);
+orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyOrder)
 orderRouter.post("/userorders", authMiddleware, userOrders);
 orderRouter.get('/list', listOrders)
